587a15db15851439390446163127a2cb7ccc3339
luoshuai <1029113525@qq.com>
2021-07-29 21:53:53 +0800

    feat:日常补充

---
+++ 数据结构/Dancer.js
@@ -0,0 +1,35 @@
+// 模拟Dancer数据:M代表女 F代表男
+let dancer = [{
+    name: "Rose",
+    sex: "M"
+}, {
+    name: "Jack",
+    sex: "F"
+}, {
+    name: "Marin",
+    sex: "M"
+}, {
+    name: "Luo",
+    sex: "F"
+}, {
+    name: "Yilisa",
+    sex: "M"
+}, {
+    name: "Bulon",
+    sex: "F"
+}, {
+    name: "eZaer",
+    sex: "F"
+}, {
+    name: "Fiaona",
+    sex: "M"
+}, {
+    name: "Naidli",
+    sex: "M"
+}, {
+    name: "Suona",
+    sex: "M"
+}, {
+    name: "Jiaoji",
+    sex: "M"
+}]
