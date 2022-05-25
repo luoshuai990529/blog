be25e938611b065a698f95487aa30623a41d8c00
luoshuai990529 <1029113525@qq.com>
2022-05-25 08:47:31 +0800

    aop

--- JavaScript/常用工具函数/utils.js
+++ JavaScript/常用工具函数/utils.js
@@ -3,7 +3,7 @@
  * @Date: 2022-05-05 16:41:18
  * @Author: luoshuai
  * @LastEditors: luoshuai
- * @LastEditTime: 2022-05-06 16:27:14
+ * @LastEditTime: 2022-05-20 15:11:16
  */
 
 /**
@@ -26,7 +26,7 @@ const getURLParameters = url => (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce
  */
 function formatUrl(url, params) {
     const curryParams = getURLParameters(url)
-    const originParams = {...curryParams, ...params}
+    const originParams = { ...curryParams, ...params }
     const paramStr = Object.keys(originParams).filter(item => originParams[item] !== '').map((item) => `${item}=${originParams[item]}`).join('&');
     return paramStr !== '' ? `${url.split('?')[0]}?${paramStr}` : `${url.split('?')[0]}`;
 }
@@ -57,3 +57,31 @@ function loadScript(src, cb) {
     };
     head.appendChild(script);
 }
+
+/* 
+    我们了解了装饰函数，它是 JavaScript 中独特的装饰者模式。
+    在写一些函数时如果是稳定且方便移植的功能，我们可以通过装饰着模式去添加一些个性化功能。
+    
+    Function.prototype.before 接受一个函数当作参数，这个函数即为新添加的函数，它装载了新添加的功能代码。
+*/
+Function.prototype.before = function (beforefn) {
+    var __self = this; // 保存原函数的引用
+    return function () { // 返回包含了原函数和新函数的"代理"函数
+        beforefn.apply(this, arguments); // 执行新函数，且保证 this 不被劫持，新函数接受的参数
+        // 也会被原封不动地传入原函数，新函数在原函数之前执行
+        return __self.apply(this, arguments); // 执行原函数并返回原函数的执行结果，
+        // 并且保证 this 不被劫持
+    }
+}
+/* 
+    Function.prototype.after 的原理跟 Function.prototype.before 一模一样，
+    唯一不同的地方在于让新添加的函数在原函数执行之后再执行。
+*/
+Function.prototype.after = function (afterfn) {
+    var __self = this;
+    return function () {
+        var ret = __self.apply(this, arguments);
+        afterfn.apply(this, arguments);
+        return ret;
+    }
+};
